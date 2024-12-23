da52aa391b5d81c864aae456904332ed6d351ba9
Ansuman Gautam <ansumangautam2002@gmail.com>
2024-12-23 12:28:03 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+// Example array
+const numbers = [34, 7, 23, 32, 5, 62];
+
+//TODO: Sorting the array in ascending order
+numbers.sort((a, b) => a - b);
+
+console.log("Ascending Order:", numbers); 
+
+//TODO: Sorting the array in descending order
+numbers.sort((a, b) => b - a);
+
+console.log("Descending Order:", numbers); 
